d3237fe0531e33ffaa310578d81dd0531bbb5116
zerovagbs5-jpg <zerovagbs5@gmail.com>
2026-03-15 01:56:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -55,5 +55,7 @@ app.post('/api/chat', (req, res) => {
     res.json(newMsg);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Engine running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
